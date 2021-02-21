29d83c576a35bb98cd3684d2aa52604e43fbb2e6
Joy Xiao <xiaoz18@mcmaster.ca>
2021-02-20 22:48:52 -0500

    keep peek() in stockpile for now

--- src/StockPile.java
+++ src/StockPile.java
@@ -7,14 +7,13 @@ public class StockPile extends Stack<Card> {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	/**
-	 * Overrides peek method since it is not allowed
-	 */
-	@Override 
-	public Card peek() {
-		return null;
-		
-	}
+//	/**
+//	 * Overrides peek method since it is not allowed
+//	 */
+//	@Override 
+//	public Card peek() {
+//		return null;
+//	}
 	
 	/**
 	 * Overrides search method since information should not be given
