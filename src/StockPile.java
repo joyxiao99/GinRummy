868f65a4f4bb5cada96a1da69c019e5c9c996644
Joy Xiao <xiaoz18@mcmaster.ca>
2021-02-20 21:40:21 -0500

    add stockpile stack method restrictions

--- src/StockPile.java
+++ src/StockPile.java
@@ -6,4 +6,23 @@ public class StockPile extends Stack<Card> {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Overrides peek method since it is not allowed
+	 */
+	@Override 
+	public Card peek() {
+		return null;
+		
+	}
+	
+	/**
+	 * Overrides search method since information should not be given
+	 * 
+	 * @returns -1
+	 */
+	@Override 
+	public int search(Object e) {
+		return -1;
+	}
 }
