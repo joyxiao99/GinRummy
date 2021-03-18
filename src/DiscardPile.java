3cd2b47132c336bf414aa5548f9793ffca92e492
Joy Xiao <xiaoz18@mcmaster.ca>
2021-03-18 19:28:41 -0400

    update discard pile comments

--- src/DiscardPile.java
+++ src/DiscardPile.java
@@ -1,13 +1,14 @@
 import java.util.Stack;
 
+/**
+ * @author Joy Xiao
+ * @brief The discard pile class 
+ */
 public class DiscardPile extends Stack<Card> {
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
-	/*
+	/**
 	 * Display the top card of the discard pile
 	 */
 	public void displayTopCard() {
