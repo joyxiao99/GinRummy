c582c423039db37674c0d2820be5d6fbdda3b437
sings59 <sings59@mcmaster.ca>
2021-03-18 19:46:04 -0400

    fixed header comment

--- src/Suit.java
+++ src/Suit.java
@@ -1,7 +1,6 @@
 /**
- * Suit enum.
- * H = Hearts, S = Spades, C = Clubs, 
- * D = Diamonds
+ * @brief Suit enum.H = Hearts, S = Spades, C = Clubs, D = Diamonds
+ * @author Smita Singh
  */
 public enum Suit{
 	H, S, C, D;
