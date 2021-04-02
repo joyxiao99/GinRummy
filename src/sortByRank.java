6e3c5352ba9652c64d9b7cb36211d5956762fc24
sings59 <sings59@mcmaster.ca>
2021-04-02 14:56:30 -0400

    added sorting by suit after rank

--- src/sortByRank.java
+++ src/sortByRank.java
@@ -6,8 +6,20 @@
 import java.util.Comparator;
 
 public class sortByRank implements Comparator<Card> {
+
+	/**
+	 * Public method that returns integer based on which card is greater in rank
+	 * 
+	 * @param c1 card one
+	 * @param c2 card two
+	 * @return integer number, if c1 is greater it returns a positive int else it returns a negative int
+	 */
 	@Override
 	public int compare(Card c1, Card c2) {
-		return c1.getRank() - c2.getRank();
+		int c;
+		c = c1.getRank()-c2.getRank();
+	    if (c == 0)
+		    c = c1.getSuit().ordinal() - c2.getSuit().ordinal();
+	    return c; 
 	} 
 }
